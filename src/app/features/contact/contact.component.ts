13a3daee11c240036c09bf6f1cef4e5cf3c7bae6
RS Business Tech <rsbusinesstech@gmail.com>
2025-07-12 20:00:44 +0530

    Starting Development

---
+++ src/app/features/contact/contact.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+   onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      alert('Message sent successfully!');
+      this.contactForm.reset();
+    }
+  }
+  
+  ngOnInit() {
+  }
+
+}
