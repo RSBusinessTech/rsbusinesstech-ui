cee5148dcd37940bbc25cfb9ab9f66a4c670aa73
RS Business Tech <rsbusinesstech@gmail.com>
2025-07-19 20:17:07 +0530

    connecting UI with Backend

--- src/app/features/contact/contact.module.ts
+++ src/app/features/contact/contact.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ContactRoutingModule } from './contact-routing.module'; 
 import { ContactComponent } from './components/contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ContactService } from './services/contact.service';
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import { ContactComponent } from './components/contact/contact.component';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    ContactRoutingModule 
-  ]
+    ContactRoutingModule ,
+    HttpClientModule           //Http Client module in order to call Rest-APIs.
+  ],
+  providers: [ContactService]  //Services (Dependency Injection).
 })
 export class ContactModule { }
