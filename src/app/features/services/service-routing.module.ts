113e3f520c84077291bb87e97a99faa8dceaefbd
RS Business Tech <rsbusinesstech@gmail.com>
2025-07-23 23:34:03 +0530

    Adding Website Details component

--- src/app/features/services/service-routing.module.ts
+++ src/app/features/services/service-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServicesComponent } from './components/services/services.component';
+import { WebsiteComponent } from './components/services/website_service/website/website.component';
 
 const routes: Routes = [
-  { path: '', component: ServicesComponent }
+  { path: '', component: ServicesComponent },        // Default route for /services
+  { path: 'website', component: WebsiteComponent }   // Route for /services/website
 ];
 
 @NgModule({
--- src/app/features/services/services.module.ts
+++ src/app/features/services/services.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicesComponent } from './components/services/services.component';
 import { ServicesRoutingModule } from './service-routing.module';
+import { WebsiteComponent } from './components/services/website_service/website/website.component';
 
 @NgModule({
-  declarations: [ServicesComponent],
+  declarations: [ServicesComponent, WebsiteComponent],
   imports: [
     CommonModule,
      ServicesRoutingModule
