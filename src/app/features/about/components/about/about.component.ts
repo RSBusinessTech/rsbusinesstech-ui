d6fe71896f77602f8b91139827e2662d34954b56
RS BusinessTech <rsbusinesstech@gmail.com>
2026-05-25 00:47:18 +0530

    redesign about us page

--- src/app/features/about/components/about/about.component.ts
+++ src/app/features/about/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
+    this.checkReveal(); // run once on load
   }
 
+  @HostListener('window:scroll', [])
+  onScroll() {
+    this.checkReveal();
+  }
+
+  checkReveal() {
+    const elements = document.querySelectorAll('.reveal');
+
+    const windowHeight = window.innerHeight;
+
+    elements.forEach((el: any) => {
+      const position = el.getBoundingClientRect().top;
+
+      if (position < windowHeight - 100) {
+        el.classList.add('active');
+      }
+    });
+  }
 }
